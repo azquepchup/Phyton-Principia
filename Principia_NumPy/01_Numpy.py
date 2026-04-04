9cef736ddf4ea76b336292ca0d94640e41274b39
azquepchup <urielsam552@gmail.com>
2026-04-04 15:59:39 -0600

    Termine la primera parte de Numpy

--- Principia_NumPy/01_Numpy.py
+++ Principia_NumPy/01_Numpy.py
@@ -47,16 +47,14 @@ y = x**2
 
 print(y)
 
-#Lo dejo en comentario para que no ejecute a cada rato y no llene la terminal
+import numpy as np
+import matplotlib.pyplot as plt
 
-#import numpy as np
-#import matplotlib.pyplot as plt
+x = np.linspace(0, 10, 100)
+y = x**2
 
-#x = np.linspace(0, 10, 100)
-#y = x**2
-
-#plt.plot(x, y)
-#plt.show()
+plt.plot(x, y)
+plt.show()
 
 print(sep)
 
@@ -98,10 +96,126 @@ print(Matriz3D.dtype) #int 64, enteros de 64bits
 
 print(Matriz3D.size) #32, 32 datos
 
-Matriz3D = np.array(Matriz3D, dtype = np.int16)
-print(Matriz3D)
+#Matriz3D = np.array(Matriz3D, dtype = np.int16) #Limita a 16bits
+#print(Matriz3D) 
 
 
 print(np.zeros((3, 2))) #* Matriz de 0, (3 Renglones,2 Columnas)
 
 print(np.ones((5,7))) #* Matriz de 1, (5 Renglones,7 Columnas)
+
+print(np.empty((2,3))) #Numeros muyyy pequeños, pero no 0 (2 Renglones,3 Columnas)
+
+print(np.arange(10)) #Py es de indice 0, comienza en 0 hasta 9 en este caso
+print(np.arange(10).reshape(2, 5)) # La misma matriz, pero en 2 filas y 5 columnas
+
+print(np.arange(10, 20, 2)) #Le que ahora inicie en 10, termine en 20, en saltos de 2 en 2, no incluye el 20 por lo mismo del indice 0
+#! Si quieisera el 20, tendria que pedirle hasta el 21
+
+print(np.linspace(10, 20, 20)) #Numeros espaciados de 10 al 20, con espacios divididos equitativamente 20 veces
+
+print(sep)
+
+from numpy import pi
+print(pi)
+
+rad = np.linspace(0, 2 * pi, 100) #Del 0 a 2pi, espaciados en 100 numeros
+
+print(rad)
+
+sen = np.sin(rad)
+print(sen)
+cos = np.cos(rad)
+print(cos)
+
+import numpy as np #! Importar las dependencias
+import matplotlib.pyplot as plt
+
+plt.plot(sen, cos)
+plt.show()
+
+
+plt.plot(rad, sen)
+plt.plot(rad, cos)
+plt.show()
+
+print(sep)
+
+a = np.linspace(10, 20, 6)
+b = np.linspace(5, 25, 6)
+print(a, b) #Me da las dos arreglos a y b, solo los da no hace nada más
+
+#Operaciones con arreglos
+
+print(a + b) #Suma los arreglos
+
+c = np.concatenate((a,b))
+print(c) #Los junta (Concatena)
+
+c.sort()
+print(c) #Los ordena de menor a mayor
+
+print(sep)
+
+#rg (radom generator)
+
+rg = np.random.default_rng(2) #generador de numeros, pero con semilla 2, es "radom", pero con una semilla especifica por lo que son los mismos datos si lo dejara sin ninguna semilla pues seria totalmente radom, ahi lo que pasa es que no hay reproducibilidad de los datos, para eso sirve la semilla
+aleatorio = rg.random(1000)
+print(aleatorio)
+plt.hist(aleatorio, bins = 100)
+plt.show()
+
+normal = rg.normal(10, 5, 100000) #Con los datos de aleatorio quiero utilizando la funcion normal, con media 10 con desvest de 5 me de 100000
+print(normal) #muestra primeros 3 y ultimos 3
+plt.hist(normal, bins = 1000)
+plt.show()
+
+enteros = rg.integers(20, size = 2000) #Enteros
+plt.hist(enteros)
+plt.show()
+
+sin_repetir = rg.choice(26, size = 10, replace = False) #Numero aleatorios sin repetir
+print(sin_repetir)
+
+estadistacos = rg.integers(20, size = 8)
+print(estadistacos.min()) #El minimo
+print(estadistacos.max()) #El maximo
+print(estadistacos.mean()) #La media
+print(estadistacos.std()) #La dvst
+print(estadistacos.sum()) #La suma
+print(estadistacos)
+
+estadistacos_2D = rg.integers(20, size = (5,4))
+print(estadistacos_2D)
+print(estadistacos_2D.min(axis = 0)) #! axis = 0, que trabaje a lo largo de las columnas, (COLUMNAS)
+print(estadistacos_2D.max(axis = 1)) #! axis = 1, que trabaje a lo largo de las filas, (FILAS)
+
+print(estadistacos_2D[estadistacos_2D < 12]) #Arreglo con solo mis numeros menores a 12, pierde la forma del arreglo, pero hace un nuevo arreglo con solo numeros menores a 12
+
+print(sep)
+
+np1 = rg.integers(20, size=(3,3))
+np2 = rg.integers(20, size=(3,3))
+np.vstack((np1, np2))
+print(np1, "\n\n", np2)
+
+print(np.vstack((np1, np2)))
+print(np.vstack((np1, np2))) #Apilamiento vertical
+print(np.hstack((np1, np2))) #Apilamiento horizontal
+
+enteros_2 = rg.integers(20, size=(10))
+print(enteros_2)
+
+print[0:6] #Me da los primeros 6 valores del arreglo
+
+print(enteros_2[0:6:2]) #Que seleccione los primeros 6 valores del arreglo de dos en dos
+print(enteros_2[::2]) #Que inicie y termine desde que comienza, hasta que termina, pero de dos en dos
+
+enteros_2D = rg.integers(20, size=(8,5))
+print(enteros_2D)
+
+print(enteros_2D[1, 3]) #Me da del segundo Renglon el cuarto valor, #! Aqui tambien aplica el indice 0, es decir no es primer renglon es segundo el primero seria 0
+
+print(enteros_2D[3:6, 1]) #! De la fila del 3 al 5, es decir, 3, 4, 5 agarra los valores de la columna 2
+
+print(enteros_2D[4:7,0:2]) #! De la fila 4 a la 6, es decir, 4, 5, 6, agarra los valores de sus columnas 1 y 2
